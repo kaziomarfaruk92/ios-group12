192f02760d585b7ba7a4b3fa3de4fe04b46d3b45
Kazi Omar Faruk <kazi.omar.faruk@bjitacademy.com>
2022-11-23 15:01:09 +0600

    Commit by omar in main

--- group12.cpp
+++ group12.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main() {
    int sea, c, n=6;
    int arr[] = {12, 35, 69, 74, 165, 54};
-   sea = 165;
+   sea = 35;
    for (c = 0; c < n; c++) {
       if (arr[c] == sea) {
-         print("Found by omar30014")
+         print("Found 35 by omar in main")
          break;
       }
    }
