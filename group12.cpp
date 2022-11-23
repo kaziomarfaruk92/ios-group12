e198a19f15594eaac90180ac01cb15b2966d13f7
Kazi Omar Faruk <kazi.omar.faruk@bjitacademy.com>
2022-11-23 14:59:14 +0600

    Commit by omar to find 74

--- group12.cpp
+++ group12.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main() {
    int sea, c, n=6;
    int arr[] = {12, 35, 69, 74, 165, 54};
-   sea = 165;
+   sea = 74;
    for (c = 0; c < n; c++) {
       if (arr[c] == sea) {
-         print("Found by omar30014")
+         print("Found 74 by omar30014")
          break;
       }
    }
