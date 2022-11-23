a71b15f59fafbb4665b9aa5956ace4e2661c62dc
kaziomarfaruk92 <118167428+kaziomarfaruk92@users.noreply.github.com>
2022-11-23 14:24:50 +0600

    Add files via upload

---
+++ group12.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+   int sea, c, n=6;
+   int arr[] = {12, 35, 69, 74, 165, 54};
+   sea = 165;
+   for (c = 0; c < n; c++) {
+      if (arr[c] == sea) {
+         print("Found in main")
+         break;
+      }
+   }
+   if (c == n)
+
+   return 0;
+}
